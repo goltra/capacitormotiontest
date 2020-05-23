3d9d8850e4b0502db8a819ad57f40ec48b73f354
goltra <goltratec@gmail.com>
2020-05-23 10:27:25 +0200

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Plugins, MotionEventResult, MotionOrientationEventResult } from '@capacitor/core';
+const { Device } = Plugins;
+const { Motion } = Plugins;
+const info = Device.getInfo();
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,50 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  accel;
+  ori;
+  infoObject
+  constructor(private zone: NgZone) {
+    let a = { nombre: 'pepe', apellido: 'garcia' };
+    console.log('a=>', a);
+    info.then(res => {
+      this.infoObject = JSON.stringify(res,null,4)
+      console.log('Device.getinfo()=>', this.infoObject);
+    })
+    // this.getMotion();
 
-  constructor() {}
+  }
+  watchAccel() {
+    const watchListener = Plugins.Motion.addListener('accel', (values) => {
+      this.zone.run(() => {
+        const v = {
+          x: values.acceleration.x.toFixed(4),
+          y: values.acceleration.y.toFixed(4),
+          z: values.acceleration.z.toFixed(4)
+        }
+        this.accel = v;
+      });
+    });
+
+    setTimeout(() => {
+      watchListener.remove();
+    }, 5000);
+  }
+
+  watchOrientation() {
+    const watchListener = Plugins.Motion.addListener('orientation', (values) => {
+      this.zone.run(() => {
+        const v = {
+          alpha: values.alpha.toFixed(4),
+          beta: values.beta.toFixed(4),
+          gamma: values.gamma.toFixed(4)
+        }
+        this.ori = v;
+      });
+    });
+    setTimeout(() => {
+      watchListener.remove();
+    }, 5000);
+  }
 
 }
